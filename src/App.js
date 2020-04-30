292dc075580162ad136a85768cca6f652f564655
Roman <pamaxa-s3@ukr.net>
2020-04-30 22:53:57 +0300

    user API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
-import Sidebar from './components/Sidebar/Sidebar';
-import Profile from './components/Profile/Profile';
-import Dialogs from './components/Dialogs/Dialogs';
+import Header from './assets/components/Header/Header';
+import Sidebar from './assets/components/Sidebar/Sidebar';
+import Profile from './assets/components/Profile/Profile';
+import Dialogs from './assets/components/Dialogs/Dialogs';
 import { Route, BrowserRouter } from 'react-router-dom';
-import News from './components/News/News';
-import Music from './components/Music/Music';
-import Contact from './components/Contact/Contact';
-import UsersContainer from './components/Users/UsersContainer';
+import News from './assets/components/News/News';
+import Music from './assets/components/Music/Music';
+import Contact from './assets/components/Contact/Contact';
+import UsersContainer from './assets/components/Users/UsersContainer';
 
 const App = () => {
 	return (
