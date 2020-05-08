e4a39f3affad7f954aeff20baf063c27f2c5b6ac
Roman <pamaxa-s3@ukr.net>
2020-05-08 22:02:43 +0300

    push for replace(technique)

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import styles from './Login.module.css';
 
+const LoginForm = (props) => {
+    return (
+        <form>
+            <div>
+                <input placeholder={'Login'} />
+            </div>
+            <div>
+                <input placeholder={'Password'} />
+            </div>
+            <div>
+                <input type='checkbox' />Remember Me
+                </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
 const Login = (props) => {
     return (
-        <h1 className={styles.login}>Login</h1>
+        <div>
+            <h1 className={styles.login}>Login</h1>
+            <LoginForm />
+        </div>
+
     )
 }
 
